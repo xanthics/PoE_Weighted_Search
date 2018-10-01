0999630ab3684884d7be715423625692298d618d
xanthics <3137623+xanthics@users.noreply.github.com>
2018-10-01 05:17:05 -0700

    Added support for 'Non-Chaos as extra Chaos Damage' amulet mod.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Usage
 
 Creates/updates jewellist.txt with a list of jewels that need to be added to Path of Building to figure out the dps values for 1 point in each stat
 
+As pointed out by github user coldino, you can edit your My Documents/Path of Building/Settings.xml directly.  The lines from jewellistxml.txt should be added directly after the `<SharedItems>` tag.  `<Shared Items>` should be right after `</Accounts>`
+
 ***
 
 **genmods** 
--- genjewels.py
+++ genjewels.py
@@ -4,16 +4,44 @@
 # Note: Requires Python 3.3.x or higher
 # This program generates all the jewels that need to be added to Path of Building
 
-header = '''
-Rarity: Rare
-{}
+
+def gentxt():
+	header = '''
+	Rarity: Rare
+	{0}
+	Prismatic Jewel
+	--------
+	Item Level: 1
+	--------
+	{0}
+	
+	'''
+
+	with open('mods.txt') as fin, open('jewellist.txt', 'w') as fout:
+		for line in fin:
+			line = line.strip('\n')
+			fout.write(header.format(line))
+
+# as pointed out by github user coldino, you can edit your My Documents/Path of Building/Settings.xml directly
+# These should be added directly after the <SharedItems> tag
+# <Shared Items> should be right after </Accounts>
+def genxml():
+	header = '''		<Item>
+			Rarity: RARE
+{0}
 Prismatic Jewel
---------
 Item Level: 1
---------
+Implicits: 0
+{0}
+		</Item>
 '''
 
-with open('mods.txt') as fin, open('jewellist.txt', 'w') as fout:
-	for line in fin:
-		line = line.strip('\n')
-		fout.write(header.format(line)+line+'\n')
+	with open('mods.txt') as fin, open('jewellistxml.txt', 'w') as fout:
+		for line in fin:
+			line = line.strip('\n')
+			fout.write(header.format(line))
+
+
+if __name__ == '__main__':
+	gentxt()
+	genxml()
