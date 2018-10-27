b0b5e67c807f0bd88634e61a02cecbd4b431c6c1
xanthics <3137623+xanthics@users.noreply.github.com>
2018-10-27 15:37:21 -0700

    Redid mod list so that the key is the mod name

--- genmods.py
+++ genmods.py
@@ -14,18 +14,26 @@ def main():
 	vals = json.load(data)
 	for i in vals['result']:
 		if i['label'] in ['Explicit', 'Implicit', 'Crafted']:
-			print(i['label'])
 			for ii in i['entries']:
 				results[i['label']][ii['id']] = ii['text']
+
+	mlist = {}
+	for label in results:
+		for val in results[label]:
+			cur = results[label][val].replace('\n', ' ')
+			if cur not in mlist:
+				mlist[cur] = [val]
+			else:
+				mlist[cur].append(val)
+
 	with open('modlist.py', 'w') as f:
 		f.write('''#!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Author: Jeremy Parks
 # Note: Requires Python 3.3.x or higher\n''')
 		f.write('mods = {\n')
-		for label in results:
-			for val in results[label]:
-				f.write('\t"{}": "{}",\n'.format(val, results[label][val].replace('\n', ' ')))
+		for label in sorted(mlist):
+			f.write('\t"{}": ["{}"],\n'.format(label, '", "'.join(mlist[label])))
 		f.write('}\n')
 
 
