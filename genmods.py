06b788f4fde75d37fd0bd542c12cecca11b024a7
xanthics <3137623+xanthics@users.noreply.github.com>
2019-05-29 18:46:07 -0700

    Added league selection to end of page.  Will use local storage to save.  Saved when query is generated.

--- genmods.py
+++ genmods.py
@@ -5,11 +5,12 @@
 
 import json
 import urllib.request
+from datetime import datetime
 
 
-def main():
+def updatemods():
 	results = {'Explicit': {}, 'Implicit': {}, 'Crafted': {}, 'Fractured': {}}
-	modurl="https://www.pathofexile.com/api/trade/data/stats"
+	modurl = "https://www.pathofexile.com/api/trade/data/stats"
 	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
 	req = urllib.request.Request(modurl, headers=headers)
 	data = urllib.request.urlopen(req)
@@ -28,17 +29,29 @@ def main():
 			else:
 				mlist[cur].append(val)
 
+	buf = ["#!/usr/bin/python", "# -*- coding: utf-8 -*-", f"# Generated: {datetime.utcnow().strftime('%m/%d/%Y(m/d/y) %H:%M:%S')} utc", 'mods = {\n']
+
+	for label in sorted(mlist):
+		mlist[label].sort()
+		buf.append('\t"{}": ["{}"],'.format(label, '", "'.join(mlist[label])))
+	buf.append('}')
+
 	with open('modlist.py', 'w') as f:
-		f.write('''#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Author: Jeremy Parks
-# Note: Requires Python 3.3.x or higher\n''')
-		f.write('mods = {\n')
-		for label in sorted(mlist):
-			mlist[label].sort()
-			f.write('\t"{}": ["{}"],\n'.format(label, '", "'.join(mlist[label])))
-		f.write('}\n')
+		f.write('\n'.join(buf))
+
+
+def updateleagues():
+	leagueurl = "http://api.pathofexile.com/leagues?realm=pc&compact=1"
+	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+	req = urllib.request.Request(leagueurl, headers=headers)
+	data = urllib.request.urlopen(req)
+	vals = json.load(data)
+
+	buf = ["#!/usr/bin/python", "# -*- coding: utf-8 -*-", f"# Generated: {datetime.utcnow().strftime('%m/%d/%Y(m/d/y) %H:%M:%S')} utc", f"leagues = {[x['id'] for x in vals if not x['id'].startswith('SSF')]}"]
+	with open('leaguelist.py', 'w') as f:
+		f.write('\n'.join(buf))
 
 
 if __name__ == "__main__":
-	main()
+	#updatemods()
+	updateleagues()
---
+++ leaguelist.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Generated: 05/30/2019(m/d/y) 01:41:10 utc
+leagues = ['Standard', 'Hardcore', 'Synthesis', 'Hardcore Synthesis', 'Synthesis Event (SRE001)', 'Synthesis Event HC (SRE002)']
