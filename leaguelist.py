79a329544e307121d4d63370c363aea0958772e5
xanthics <3137623+xanthics@users.noreply.github.com>
2019-06-03 17:10:21 -0700

    Updated league list

--- leaguelist.py
+++ leaguelist.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Generated: 05/30/2019(m/d/y) 01:41:10 utc
-leagues = ['Standard', 'Hardcore', 'Synthesis', 'Hardcore Synthesis', 'Synthesis Event (SRE001)', 'Synthesis Event HC (SRE002)']
+# Generated: 06/04/2019(m/d/y) 00:09:58 utc
+leagues = ['Standard', 'Hardcore']
