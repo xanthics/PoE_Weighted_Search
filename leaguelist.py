ad1d92b9c8a361405c168634266fffb4b8f4d8e3
xanthics <3137623+xanthics@users.noreply.github.com>
2019-09-16 11:57:19 -0700

    Jwel list update, mod update, removed rogue print statement

--- leaguelist.py
+++ leaguelist.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Generated: 09/07/2019(m/d/y) 20:18:25 utc
+# Generated: 09/16/2019(m/d/y) 18:56:03 utc
 leagues = ['Standard', 'Hardcore', 'Blight', 'Hardcore Blight']
