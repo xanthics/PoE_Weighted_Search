2f5f8e7d6875cd155c9b339bd1ef95a3c5c3d3f8
xanthics <3137623+xanthics@users.noreply.github.com>
2019-12-08 19:30:45 -0800

    Added excerpt from conversation regarding possible future of this tool.

--- leaguelist.py
+++ leaguelist.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Generated: 09/16/2019(m/d/y) 18:56:03 utc
-leagues = ['Standard', 'Hardcore', 'Blight', 'Hardcore Blight']
+# Generated: 12/09/2019(m/d/y) 03:29:08 utc
+leagues = ['Standard', 'Hardcore']
