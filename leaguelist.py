9d49bb35ecc9083657d05787d03547a8d29c3c1b
xanthics <3137623+xanthics@users.noreply.github.com>
2020-03-13 16:10:23 -0700

    Updated league list

--- leaguelist.py
+++ leaguelist.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Generated: 12/14/2019(m/d/y) 07:57:22 utc
-leagues = ['Standard', 'Hardcore', 'Metamorph', 'Hardcore Metamorph']
+# Generated: 03/13/2020(m/d/y) 23:08:10 utc
+leagues = ['Standard', 'Hardcore', 'Delirium', 'Hardcore Delirium']
