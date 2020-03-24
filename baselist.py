12f7c039889bbeb386fe51c895f6840350d54b12
xanthics <3137623+xanthics@users.noreply.github.com>
2020-03-24 14:13:56 -0700

    added list of valid bases for user selection

---
+++ baselist.py
@@ -0,0 +1,16 @@
+# lists that controls the order in which options are presented to the user
+bases = [
+	"All Jewel",
+	"Jewel",
+	"AbyssJewel",
+	"Caster Weapon",
+	"Amulet",
+	"Ring",
+	"Belt",
+	"Quiver",
+	"Shield",
+	"Gloves",
+	"Helmet",
+	"Body Armour",
+	"Boots",
+]
