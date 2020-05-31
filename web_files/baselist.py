037c58cd81de6f5309adf88bc1f57c8d9f6eb9a9
xanthics <3137623+xanthics@users.noreply.github.com>
2020-05-31 14:04:37 -0700

    Seeing if folders is a cdn issue.  Will try updated index later.

---
+++ web_files/baselist.py
@@ -0,0 +1,18 @@
+# lists that controls the order in which options are presented to the user
+bases = [
+	"All Jewel",
+	"Base Jewel",
+	"Abyss Jewel",
+	"Caster Weapon",
+	"Spellslinger MH",
+	"Spellslinger DW",
+	"Amulet",
+	"Ring",
+	"Belt",
+	"Quiver",
+	"Shield",
+	"Gloves",
+	"Helmet",
+	"Body Armour",
+	"Boots",
+]
---
+++ web_files/leaguelist.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Generated: 05/31/2020(m/d/y) 19:37:07 utc
+leagues = ['Standard', 'Hardcore', 'Delirium', 'Hardcore Delirium']
