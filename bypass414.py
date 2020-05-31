8ccfdaa7c8f583f7af5c9495895b9005f3b92d6a
xanthics <3137623+xanthics@users.noreply.github.com>
2020-05-31 11:58:47 -0700

    changed patreon to image, fixed page breaking bug.

--- bypass414.py
+++ bypass414.py
@@ -2,15 +2,15 @@ from urllib import request
 import webbrowser
 import json
 
-url = "https://www.pathofexile.com/api/trade/search/Delve"
+url = "https://www.pathofexile.com/api/trade/exchange/Delirium"
 header = {'Content-type': 'application/json',
 		  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
 
-data = input("Enter json query from site: ")
+data = '''{"exchange":{"want":["chaos"],"have":["exa"],"status":"online"}}'''  # input("Enter json query from site: ")
 
 req = request.Request(url, data.encode(), header)
 
-resp = request.urlopen(req)
-
-webbrowser.open_new_tab('https://www.pathofexile.com/trade/search/Betrayal/{}'.format(json.load(resp)['id']))
+resp = json.load(request.urlopen(req))
+print(resp)
+#webbrowser.open_new_tab('https://www.pathofexile.com/trade/exchange/Delirium/{}'.format(resp['id']))
 
