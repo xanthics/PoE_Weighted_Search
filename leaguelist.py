e2f4f0f617e35d7bd282082fb95fb48549b09355
xanthics <3137623+xanthics@users.noreply.github.com>
2020-12-21 06:00:54 -0800

    Bugfix, forgot to upload modsjson.py

--- leaguelist.py
+++ leaguelist.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Generated: 09/18/2020(m/d/y) 21:16:15 utc
-leagues = ['Standard', 'Hardcore', 'Heist', 'Hardcore Heist']
+# Generated: 12/21/2020(m/d/y) 13:59:19 utc
+leagues = ['Standard', 'Hardcore', 'Heist', 'Hardcore Heist', 'Flashback (DRE007)', 'Flashback HC (DRE008)', 'Flashback SSF (DRE009)', 'Flashback HC SSF (DRE010)']
