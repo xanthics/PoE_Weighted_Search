39b823dddf5d7a13933c652f7ee47d1588755d47
xanthics <3137623+xanthics@users.noreply.github.com>
2022-05-17 16:25:24 -0700

    update leagues

--- leaguelist.py
+++ leaguelist.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Generated: 02/05/2022(m/d/y) 18:23:08 utc
-leagues = ['Standard', 'Hardcore', 'Archnemesis', 'Hardcore Archnemesis']
+# Generated: 05/17/2022(m/d/y) 16:13:08 utc
+leagues = ['Standard', 'Hardcore', 'Sentinel', 'Hardcore Sentinel']
